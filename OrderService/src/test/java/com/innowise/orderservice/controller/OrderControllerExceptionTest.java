59198725bd80c3c742fed3c558976b7ad59d5475
dshparko <951008d@mail.ru>
2025-10-23 23:31:12 +0300

    feat: add tests

---
+++ OrderService/src/test/java/com/innowise/orderservice/controller/OrderControllerExceptionTest.java
@@ -0,0 +1,50 @@
+package com.innowise.orderservice.controller;
+
+
+import com.innowise.orderservice.exception.ResourceNotFoundException;
+import com.innowise.orderservice.service.OrderService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderControllerExceptionTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private OrderService orderService;
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void getOrderById_shouldReturn404_whenNotFound() throws Exception {
+        Mockito.when(orderService.getOrderById(anyLong()))
+                .thenThrow(new ResourceNotFoundException("Order not found"));
+
+        mockMvc.perform(get("/api/v1/orders/99"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error").value("Order not found"))
+                .andExpect(jsonPath("$.status").value(404));
+    }
+
+    @Test
+    void deleteOrder_shouldReturn401_whenUnauthorized() throws Exception {
+        mockMvc.perform(delete("/api/v1/orders/5"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.status").value(401))
+                .andExpect(jsonPath("$.error").exists());
+    }
+}
