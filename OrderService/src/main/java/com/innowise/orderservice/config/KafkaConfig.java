61774f91e2656c42f535c8177954a132b0d92ea2
dshparko <951008d@mail.ru>
2025-11-21 15:37:46 +0300

    fix: fix kafka config

--- OrderService/src/main/java/com/innowise/orderservice/config/KafkaConfig.java
+++ OrderService/src/main/java/com/innowise/orderservice/config/KafkaConfig.java
@@ -2,19 +2,24 @@ package com.innowise.orderservice.config;
 
 import com.innowise.orderservice.model.dto.OrderEvent;
 import com.innowise.orderservice.model.dto.PaymentEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
-import org.springframework.util.backoff.FixedBackOff;
+import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,42 +27,92 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    private static final String AUTO_OFFSET_RESET = "earliest";
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
     @Bean
     public ProducerFactory<String, OrderEvent> orderEventProducerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
-
-        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
-        configProps.put(ProducerConfig.RETRIES_CONFIG, 3);
-        configProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
-        configProps.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5);
-
-        return new DefaultKafkaProducerFactory<>(configProps);
+        return new DefaultKafkaProducerFactory<>(commonProducerConfigs());
     }
 
     @Bean
-    public KafkaTemplate<String, OrderEvent> orderEventKafkaTemplate() {
-        return new KafkaTemplate<>(orderEventProducerFactory());
+    public KafkaTemplate<String, OrderEvent> orderEventKafkaTemplate(
+            ProducerFactory<String, OrderEvent> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    private Map<String, Object> commonProducerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5);
+
+        return props;
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, PaymentEvent> kafkaListenerContainerFactory(
-            ConsumerFactory<String, PaymentEvent> consumerFactory) {
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentEvent> paymentEventKafkaListenerContainerFactory(
+            KafkaTemplate<String, PaymentEvent> paymentEventKafkaTemplate) {
+
+        JsonDeserializer<PaymentEvent> deserializer = new JsonDeserializer<>(PaymentEvent.class);
+        deserializer.addTrustedPackages("*");
+        deserializer.setRemoveTypeHeaders(true);
+        deserializer.setUseTypeMapperForKey(false);
+
+        DefaultKafkaConsumerFactory<String, PaymentEvent> consumerFactory =
+                new DefaultKafkaConsumerFactory<>(commonConsumerConfigs(),
+                        new StringDeserializer(), deserializer);
+
         ConcurrentKafkaListenerContainerFactory<String, PaymentEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
 
-        factory.setCommonErrorHandler(new DefaultErrorHandler(
-                new DeadLetterPublishingRecoverer(orderEventKafkaTemplate()),
-                new FixedBackOff(1000L, 3)
-        ));
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
+                new DeadLetterPublishingRecoverer(paymentEventKafkaTemplate),
+                new ExponentialBackOffWithMaxRetries(5)
+        );
+        errorHandler.setAckAfterHandle(false);
+        errorHandler.addNotRetryableExceptions(IllegalArgumentException.class);
+
+        factory.setCommonErrorHandler(errorHandler);
 
         return factory;
     }
 
+    private Map<String, Object> commonConsumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, AUTO_OFFSET_RESET);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 300000);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 500);
+        props.put(ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);
+
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, PaymentEvent> paymentEventProducerFactory() {
+        Map<String, Object> props = new HashMap<>(commonProducerConfigs());
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PaymentEvent> paymentEventKafkaTemplate(
+            ProducerFactory<String, PaymentEvent> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
 }
